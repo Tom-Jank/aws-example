5efbd0fe9ed9371e762599051d76f8dccaeb25f7
tjank <tjank@deloittece.com>
2023-08-09 14:17:51 +0200

    add model

--- src/main/java/com/aws/example/resource/HelloResource.java
+++ src/main/java/com/aws/example/resource/HelloResource.java
@@ -1,16 +1,26 @@
 package com.aws.example.resource;
 
+import model.MessageModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api")
+@CrossOrigin("*")
 public class HelloResource {
 
-    @GetMapping
-    public String responseEntity() {
-        return "Hello";
+    @GetMapping("/hello")
+    public ResponseEntity<MessageModel> helloMessage() {
+        return new ResponseEntity<>(new MessageModel("Hello"), HttpStatus.OK);
+    }
+
+    @GetMapping("/goodbye")
+    public ResponseEntity<MessageModel> goodbyeMessage() {
+        return new ResponseEntity<>(new MessageModel("Goodbye"), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/model/MessageModel.java
@@ -0,0 +1,18 @@
+package model;
+
+public class MessageModel {
+
+    private String message;
+
+    public MessageModel(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
