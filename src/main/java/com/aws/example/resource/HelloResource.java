f0160f40401c2ad82f7eb9c7890637e8eb460673
tjank <tjank@deloittece.com>
2023-08-09 13:50:37 +0200

    quickfix

--- src/main/java/com/aws/example/resource/HelloResource.java
+++ src/main/java/com/aws/example/resource/HelloResource.java
@@ -1,18 +1,16 @@
 package com.aws.example.resource;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloResource {
 
     @GetMapping
     public String responseEntity() {
-        return "Hello";
+        return "Dupa";
     }
 
 }
