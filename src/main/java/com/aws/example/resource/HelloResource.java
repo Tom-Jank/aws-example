4b0008367c83077845683f7781c3a4787f7ba71f
tjank <tjank@deloittece.com>
2023-08-09 13:43:08 +0200

    add cors configuration

--- src/main/java/com/aws/example/resource/HelloResource.java
+++ src/main/java/com/aws/example/resource/HelloResource.java
@@ -1,11 +1,13 @@
 package com.aws.example.resource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloResource {
 
     @GetMapping
